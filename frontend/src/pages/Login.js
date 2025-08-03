bb3c1654f6541712a62afb59d965ef008d39afcc
AMIRTHAM <155797726+AMIRTHAM23@users.noreply.github.com>
2025-08-03 15:42:46 +0530

    Update Login.js

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -12,7 +12,7 @@ function Login() {
   const handleSubmit = async e => {
     e.preventDefault();
     try {
-      const res = await axios.post('http://localhost:5000/api/users/login', form);
+      const res = await axios.post('https://prodigy-fs-01-h4t8.onrender.com', form);
       localStorage.setItem('token', res.data.token);
       localStorage.setItem('user', JSON.stringify(res.data.user));
       toast.success('Login successful');
