98ba5d397827992915673ec32f01a2a8e8f53c93
AMIRTHAM <155797726+AMIRTHAM23@users.noreply.github.com>
2025-08-03 15:43:06 +0530

    Update Register.js

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -12,7 +12,7 @@ function Register() {
   const handleSubmit = async e => {
     e.preventDefault();
     try {
-      const res = await axios.post("https://prodigy-fs-01-h4t8.onrender.com", data);
+      const res = await axios.post("https://prodigy-fs-01-h4t8.onrender.com",form);
       localStorage.setItem('token', res.data.token);
       localStorage.setItem('user', JSON.stringify(res.data.user));
       toast.success('Registered successfully');
